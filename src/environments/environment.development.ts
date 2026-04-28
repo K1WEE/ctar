991ed5aa3a607dc968e5310d67748f5ef2d7f207
jakkapat.pan@kkumail.com <jakkapat.pan@kkumail.com>
2026-04-28 14:27:28 +0700

    chore: remove environments from git tracking

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  supabase: {
-    url: 'https://jqolbyhjvslqzbdwcubs.supabase.co',
-    key: 'sb_publishable_Fu8IZqKTRl5cbSXgXwFPGA_g_tH1eja'
-  }
-};
